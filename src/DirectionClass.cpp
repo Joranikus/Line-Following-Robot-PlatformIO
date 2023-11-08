46cd7d1b4133bdfdafa9a90dfa08f982dc29f811
KineVictori <kine.aurora@hotmail.com>
2023-11-08 11:01:08 +0100

    something if

--- src/DirectionClass.cpp
+++ src/DirectionClass.cpp
@@ -1,6 +1,3 @@
-//
-// Created by oyste on 9/18/2023.
-//
 
 #include <Arduino.h>
 #include "DirectionClass.hpp"
@@ -27,12 +24,36 @@ namespace dir {
         //Gets current direction from sensor, and updates the list of prev values.
         DirectionClass::readSensorPins();
 
-        outPins;
-        if ;
-        int left = 50;
-        int right = 255;
+        bool bigLeft = outPins[4];
+        bool smallLeft = outPins[3];
+        bool front = outPins[2];
+        bool smallRight = outPins[1];
+        bool bigRight = outPins[0];
 
+        if (bigLeft) {
+            int left = 70;
+            int right = 255;
+        }
 
+        if (smallLeft) {
+            int left = 150;
+            int right = 255;
+        }
+
+        if (front) {
+            int left = 255;
+            int right = 255;
+        }
+
+        if (smallRight) {
+            int left = 255;
+            int right = 150;
+        }
+
+        if (bigRight) {
+            int left = 255;
+            int right = 70;
+        }
         return {left, right};
     }
 
