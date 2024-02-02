fc41b5e39da8a010d54098a4b268a73203290ab0
Øystein Bringsli <oysteinmb@gmail.com>
2024-02-02 21:31:34 +0100

    Changed scope of class

--- include/DirectionClass.hpp
+++ include/DirectionClass.hpp
@@ -15,33 +15,15 @@ namespace dir {
         double get_direction();
 
     private:
-        double past_directions[100] = {0};
-        int length_of_past_dir = 100;
 
         static int arr_sum(const int* inPins, int size);
         double this_direction();
-        static double weighted(int max, int ix, double value);
-        void update_past_directions(double value);
 
         int antallPins;
-        int sensorPins[5];
-        int outPins[5];
-        int sensorLimit = 500;
+        int sensorPins[7];
+        int outPins[7];
         void readSensorPins();
-
-        double pid_SP = 0.0;
-        double pid_Kp = 0.8;
-        double pid_Ki = 0.0;
-        double pid_Kd = 0.0;
-
         double prev_direction = 0.0;
-        double prev_integral = 0.0;
-        unsigned long prev_timestep = millis();
-        unsigned long dt = 0.0;
-
-        double get_proposional();
-        double get_integral();
-        double get_derived();
     };
 
 } // dir
--- src/DirectionClass.cpp
+++ src/DirectionClass.cpp
@@ -21,63 +21,11 @@ namespace dir {
             pinMode(pin, INPUT);
         }
 
-        prev_timestep = millis();
     }
 
     double DirectionClass::get_direction() {
-
-        //Gets current direction from sensor, and updates the list of prev values.
         DirectionClass::readSensorPins();
-        double new_value = DirectionClass::this_direction();
-        DirectionClass::update_past_directions(new_value);
-
-        //return past_directions[length_of_past_dir - 1];
-
-        unsigned long new_time = millis();
-        dt = new_time - prev_timestep;
-        prev_timestep = new_time;
-
-
-        //Propsional vil være Kp * [-1 til 1]
-        //=> PID_value vil ikke alltid vil være mellom -1 og 1
-        //Dermed må den på ett eller anna vis bli normalisert
-        //Så vi faktisk kan plusse på integral og derived.
-        //Eks: proposional = 1, integral = 1 => PID_value = 2
-
-        double prop_part = get_proposional();
-        double inte_part = get_integral();
-        double derv_part = get_derived();
-
-        double PID_value = prop_part + inte_part + derv_part;
-        //Serial.print("Prop: ");
-        //Serial.print(prop_part);
-        //Serial.print(" | Inte: ");
-        //Serial.print(inte_part);
-        //Serial.print(" | Derv: ");
-        //Serial.print(derv_part);
-        //Serial.print(" | Tot: ");
-        //Serial.println(PID_value);
-
-        return PID_value;
-    }
-
-    void DirectionClass::update_past_directions(double value) {
-        //Shifts the whole list 1 spot, discarding the oldest value, and making room for the new one.
-        for (int i = 0; i < (length_of_past_dir - 1); i++)
-        {
-            past_directions[i] = past_directions[i + 1];
-        }
-        past_directions[length_of_past_dir - 1] = value;
-    }
-
-    double DirectionClass::weighted(int max, int ix, double value) {
-        //Returns a weighted value based on position in list.
-        //Returns value * (ix ^ 4 / max ^ 4)
-
-        double num = ix * ix * ix * ix;
-        double den = max * max * max * max;
-
-        return value * (num / den);
+        return DirectionClass::this_direction();
     }
 
     int DirectionClass::arr_sum(const int* arr, int size)
@@ -95,7 +43,7 @@ namespace dir {
     double DirectionClass::this_direction() {
 
         int as = arr_sum(outPins, antallPins);
-        if ((as == 0) or (as == antallPins)){return prev_direction;}
+        if ((as == 0) or (as == antallPins)) {return prev_direction;}
 
         //Sets the upper and lower bound to the edges.
         int minIx = antallPins;
@@ -108,54 +56,20 @@ namespace dir {
             if ((i < minIx) and outPins[i]){minIx = i;}
         }
 
-        //Shifts the bounds from [0, antallPins - 1] to [0, max_val] => [-1, 1]
-        double max_val = 2.0;
-        double minIxDouble = max_val * static_cast<double>(minIx) / (static_cast<double>(antallPins) - 1.0) - 1.0;
-        double maxIxDouble = max_val * static_cast<double>(maxIx) / (static_cast<double>(antallPins) - 1.0) - 1.0;
+        auto avg_bound = static_cast<double>((minIx + maxIx)) / 2.0;
+        auto normalized = avg_bound / static_cast<double>(antallPins - 1);
+        auto shifted = normalized * 300.0;
 
-        //Returns the lower bound plus half the difference. (Returns the number in the middle of the bounds)
-        double tmp = minIxDouble + (maxIxDouble - minIxDouble) / 2.0;
-
-        prev_direction = tmp;
-        return tmp;
+        prev_direction = shifted;
+        return shifted;
     }
 
     void DirectionClass::readSensorPins() {
-
-        //Serial.println(analogRead(sensorPins[0]));
         for (int i = 0; i < antallPins; i++)
         {
-            outPins[i] = analogRead(sensorPins[i]) > sensorLimit;
+            outPins[i] = digitalRead(sensorPins[i]);
         }
     }
 
-    double DirectionClass::get_proposional() {
-        return pid_Kp * (past_directions[length_of_past_dir - 1] - pid_SP);
-    }
-
-    double DirectionClass::get_integral() {
-        if ((past_directions[length_of_past_dir - 1] - pid_SP) * prev_integral < 0) {prev_integral = 0;}
-        if ((past_directions[length_of_past_dir - 1] - pid_SP) == 0) {prev_integral = 0;}
-
-        prev_integral += pid_Ki * (past_directions[length_of_past_dir - 1] - pid_SP) * dt;
-        return prev_integral;
-
-//        double sum = 0;
-//        for (double val : past_directions)
-//        {
-//            if (sum * val < 0){sum = 0;}
-//            else {sum += val;}
-//        }
-//
-//        return pid_Ki * sum;
-    }
-
-    double DirectionClass::get_derived() {
-        double out = (past_directions[length_of_past_dir - 1]
-                - past_directions[length_of_past_dir - 2])
-                        / dt;
-
-        return pid_Kd * out;
-    }
 
 } // dir
