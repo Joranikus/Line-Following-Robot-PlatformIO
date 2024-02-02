4d942e3e153770e75f642e7fa626d7c046d1935f
Øystein Bringsli <oysteinmb@gmail.com>
2024-02-02 21:18:25 +0100

    Added test class

---
+++ include/Tests.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Øystein Bringsli.
+//
+
+#ifndef LINEFOLLOWINGROBOT_TESTS_HPP
+#define LINEFOLLOWINGROBOT_TESTS_HPP
+
+
+class Tests {
+
+};
+
+
+#endif //LINEFOLLOWINGROBOT_TESTS_HPP
