d881099fa69ead4c8894172c5458fc19db1144b5
Øystein Bringsli <oysteinmb@gmail.com>
2024-02-05 13:50:30 +0100

    Turned off pin 4

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -9,11 +9,15 @@
 void Tests::printSensors(const int *pins, const int numPins) {
     for (int ix = 0; ix < numPins; ix++)
     {
+        //Pin 4 is turned off!!!!
+
         Serial.print(digitalRead(pins[ix]));
 
         if (ix + 1 < numPins) {
             Serial.print("|");
         }
+
+        //Pin 4 is turned off!!!!
     }
 
     Serial.println();
