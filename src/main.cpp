644dca4b1437fb9c44ac1af0f1bb8785e0a4fb53
Øystein Bringsli <oysteinmb@gmail.com>
2024-02-05 13:51:19 +0100

    Setup pwm, and using tests

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,32 @@ Tests tests;
 
 void setup()
 {
+
+    ledcSetup(0, 300000, 8);
+    ledcSetup(1, 300000, 8);
+
+    ledcAttachPin(25, 0);
+    ledcAttachPin(26, 1);
+
     Serial.begin(9600);
     Serial.println();
     Serial.println("Setup complete.");
 }
 
+unsigned long lastPrintTime = 0;
 void loop()
 {
     auto startTime = millis();
 
     auto dir = directionClass.get_direction();
-    motorController.motorControl(dir, 0.7);
+    motorController.motorControl(dir, 1.0);
 
     auto endTime = millis();
     motorController.PrintMotorSpeed(dir, endTime - startTime);
+
+    auto currentTime = millis();
+    if (currentTime > (lastPrintTime + 500)) {
+        tests.printSensors(sensorPins, numSensorPins);
+        lastPrintTime = currentTime;
+    }
 }
