58b9bd355b47b8d2fe26c19d3ba19db32944d9bf
Øystein Bringsli <oysteinmb@gmail.com>
2024-02-05 10:34:34 +0100

    Uses directionClass

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 
 #include <Arduino.h>
 
+#include "DirectionClass.hpp"
 #include "MotorController.hpp"
 #include "PID.hpp"
 
 int antallPins = 7;
 int sensorPins[7] = {22, 23, 16, 17, 5, 18, 21};
-float prev_dir;
 
+
+DirectionClass directionClass{sensorPins, antallPins};
 PID pid;
 MotorController motorController{0, 300};
 
@@ -22,23 +24,7 @@ void loop()
 {
     auto startTime = millis();
 
-    bool sensor_activations[7];
-    int sum = 0;
-    for (int i = 0; i < antallPins; i++)
-    {
-        sensor_activations[i] = digitalRead(sensorPins[i]);
-        sum += sensor_activations[i];
-
-    }
-
-    float dir;
-    if ((sum == 0) || (sum == antallPins)) {
-        dir = prev_dir;
-    } else {
-        dir = direction(sensor_activations, antallPins);
-        prev_dir = dir;
-    }
-
+    auto dir = directionClass.get_direction();
     motorController.motorControl(dir, 0.7);
 
     auto endTime = millis();
